dcba6aa1b6262e98d2cd067e8237290b3aa9ae62
Tawba Abdallah <tawba222@proton.me>
2026-08-23 12:13:40 +0300

    sol:Container with most water problem solution using java

---
+++ ContainerWithMostWater/ContainerWithMostWater.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxArea(int[] heights) {
+        int max=0;
+        int l=0;
+        int r= heights.length-1;
+        while(l<r){
+            int area=Math.min(heights[l],heights[r])*(r-l);
+            max=Math.max(max,area);
+            if (heights[r]>heights[l]){
+                l++;
+            }else{
+                r--;
+            }
+        }
+        return max;
+    }
+
+}
