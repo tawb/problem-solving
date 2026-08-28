5ad2d80b8e4a89bf64027a447a2c950bde0f55b6
Tawba Abdallah <tawba222@proton.me>
2026-08-28 20:53:05 +0300

    sol:Binary Search problems solutions using java

---
+++ BinarySearch/Find_Minimum_In_Rotated_SortedArray/Find_Minimum_In_Rotated_SortedArray.java
@@ -0,0 +1,21 @@
+//first sol Binary Search
+class Solution {
+    public int findMin(int[] nums) {
+        int res=nums[0];
+        int l=0;
+        int r=nums.length-1;
+        while (l<=r){
+            if (nums[l]< nums[r]){
+                res=Math.min(res,nums[l]);
+                break;}
+            int m=l+(r-l)/2;
+            res=Math.min(res,nums[m]);
+            if (nums[m]>=nums[l]){
+                l=m+1;
+            }else{
+                r=m-1;
+            }
+        }
+        return res;
+    }
+}
